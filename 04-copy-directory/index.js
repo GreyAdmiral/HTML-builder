e3429a96fe7b3aec139c4bd64f1fd243d3cd47e8
Binarion <itmaster@duck.com>
2024-01-23 18:26:17 +0300

    refactor: create function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,67 +1,71 @@
-const path = require('path');
-const fs = require('fs');
+function folderSinchron() {
+  const path = require('path');
+  const fs = require('fs');
 
-const folder = path.join(__dirname, 'files');
-const copyFolder = path.join(__dirname, 'files-copy');
+  const folder = path.join(__dirname, 'files');
+  const copyFolder = path.join(__dirname, 'files-copy');
 
-fs.mkdir(copyFolder, { recursive: true }, (err) => {
-  if (err) {
-    throw err;
-  }
-});
-
-fs.readdir(folder, (err, files) => {
-  if (err) {
-    throw err;
-  }
-
-  files.forEach((it) => {
-    fs.copyFile(path.join(folder, it), path.join(copyFolder, it), (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  });
-});
-
-fs.watch(folder, (event, file) => {
-  if (file && event == 'change') {
-    const stream = fs.createReadStream(path.join(folder, file), {
-      encoding: 'utf-8',
-    });
-    stream.once('data', (cnt) => {
-      const wrStream = fs.createWriteStream(path.join(copyFolder, file));
-      wrStream.write(cnt);
-      wrStream.end();
-    });
-    stream.once('error', (err) => {
+  fs.mkdir(copyFolder, { recursive: true }, (err) => {
+    if (err) {
       throw err;
-    });
-  } else if (event == 'rename') {
-    fs.access(path.join(folder, file), (err) => {
-      const isF = !err;
+    }
+  });
 
-      fs.access(path.join(copyFolder, file), (err) => {
-        const isCpF = !err;
+  fs.readdir(folder, (err, files) => {
+    if (err) {
+      throw err;
+    }
 
-        if (isF) {
-          fs.copyFile(
-            path.join(folder, file),
-            path.join(copyFolder, file),
-            (err) => {
-              if (err) {
-                throw err;
-              }
-            },
-          );
-        } else if (isCpF) {
-          fs.rm(path.join(copyFolder, file), (err) => {
-            if (err) {
-              throw err;
-            }
-          });
+    files.forEach((it) => {
+      fs.copyFile(path.join(folder, it), path.join(copyFolder, it), (err) => {
+        if (err) {
+          throw err;
         }
       });
     });
-  }
-});
+  });
+
+  fs.watch(folder, (event, file) => {
+    if (file && event == 'change') {
+      const stream = fs.createReadStream(path.join(folder, file), {
+        encoding: 'utf-8',
+      });
+      stream.once('data', (cnt) => {
+        const wrStream = fs.createWriteStream(path.join(copyFolder, file));
+        wrStream.write(cnt);
+        wrStream.end();
+      });
+      stream.once('error', (err) => {
+        throw err;
+      });
+    } else if (event == 'rename') {
+      fs.access(path.join(folder, file), (err) => {
+        const isF = !err;
+
+        fs.access(path.join(copyFolder, file), (err) => {
+          const isCpF = !err;
+
+          if (isF) {
+            fs.copyFile(
+              path.join(folder, file),
+              path.join(copyFolder, file),
+              (err) => {
+                if (err) {
+                  throw err;
+                }
+              },
+            );
+          } else if (isCpF) {
+            fs.rm(path.join(copyFolder, file), (err) => {
+              if (err) {
+                throw err;
+              }
+            });
+          }
+        });
+      });
+    }
+  });
+}
+
+folderSinchron();
