17dfd821abc74d0d70c81aa515a4be2d81743564
Binarion <itmaster@duck.com>
2024-01-24 06:06:58 +0300

    fix: fixed the algorithm

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -25,46 +25,24 @@ function folderSinchron() {
     });
   });
 
-  fs.watch(folder, (event, file) => {
-    if (file && event == 'change') {
-      const stream = fs.createReadStream(path.join(folder, file), {
-        encoding: 'utf-8',
-      });
-      stream.once('data', (cnt) => {
-        const wrStream = fs.createWriteStream(path.join(copyFolder, file));
-        wrStream.write(cnt);
-        wrStream.end();
-      });
-      stream.once('error', (err) => {
-        throw err;
-      });
-    } else if (event == 'rename') {
-      fs.access(path.join(folder, file), (err) => {
-        const isF = !err;
-
-        fs.access(path.join(copyFolder, file), (err) => {
-          const isCpF = !err;
-
-          if (isF) {
-            fs.copyFile(
-              path.join(folder, file),
-              path.join(copyFolder, file),
-              (err) => {
-                if (err) {
-                  throw err;
-                }
-              },
-            );
-          } else if (isCpF) {
-            fs.rm(path.join(copyFolder, file), (err) => {
-              if (err) {
-                throw err;
-              }
-            });
-          }
-        });
-      });
+  fs.readdir(copyFolder, (err, files) => {
+    if (err) {
+      throw err;
     }
+
+    files.forEach((it) => {
+      fs.access(path.join(folder, it), (err) => {
+        const isNotSource = !!err;
+
+        if (isNotSource) {
+          fs.rm(path.join(copyFolder, it), (err) => {
+            if (err) {
+              throw err;
+            }
+          });
+        }
+      });
+    });
   });
 }
 
